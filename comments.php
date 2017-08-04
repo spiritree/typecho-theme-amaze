173aa82d0998ec875e7ea4741bbcc80092aa8658
spiritree <spiritreess@gmail.com>
2017-08-04 13:45:34 +0800

    update comment style

--- comments.php
+++ comments.php
@@ -21,18 +21,20 @@
             <?php if($this->user->hasLogin()): ?>
     		<p><?php _e('登录身份: '); ?><a href="<?php $this->options->profileUrl(); ?>"><?php $this->user->screenName(); ?></a>. <a href="<?php $this->options->logoutUrl(); ?>" title="Logout"><?php _e('退出'); ?> &raquo;</a></p>
             <?php else: ?>
-    		<p>
-                <label for="author" class="required"><?php _e('称呼'); ?></label>
-    			<input type="text" name="author" id="author" class="text" value="<?php $this->remember('author'); ?>" required />
-    		</p>
-    		<p>
-                <label for="mail"<?php if ($this->options->commentsRequireMail): ?> class="required"<?php endif; ?>><?php _e('Email'); ?></label>
-    			<input type="email" name="mail" id="mail" class="text" value="<?php $this->remember('mail'); ?>"<?php if ($this->options->commentsRequireMail): ?> required<?php endif; ?> />
-    		</p>
-    		<p>
-                <label for="url"<?php if ($this->options->commentsRequireURL): ?> class="required"<?php endif; ?>><?php _e('网站'); ?></label>
-    			<input type="url" name="url" id="url" class="text" placeholder="<?php _e('http://'); ?>" value="<?php $this->remember('url'); ?>"<?php if ($this->options->commentsRequireURL): ?> required<?php endif; ?> />
-    		</p>
+    		<div class="am-g">
+    			<span class="am-u-lg-4">
+    				<label for="author" class="required"><?php _e('称呼'); ?></label>
+    				<input type="text" name="author" id="author" class="text" value="<?php $this->remember('author'); ?>" required />
+    			</span>
+    			<span class="am-u-lg-4">
+    				<label for="mail"<?php if ($this->options->commentsRequireMail): ?> class="required"<?php endif; ?>><?php _e('Email'); ?></label>
+    				<input type="email" name="mail" id="mail" class="text" value="<?php $this->remember('mail'); ?>"<?php if ($this->options->commentsRequireMail): ?> required<?php endif; ?> />
+    			</span>
+    			<span class="am-u-lg-4">
+    				<label for="url"<?php if ($this->options->commentsRequireURL): ?> class="required"<?php endif; ?>><?php _e('网站'); ?></label>
+    				<input type="url" name="url" id="url" class="text" value="<?php $this->remember('url'); ?>"<?php if ($this->options->commentsRequireURL): ?> required<?php endif; ?> />
+    			</span>
+    		</div>
             <?php endif; ?>
     		<p>
                 <label for="textarea" class="required"><?php _e('内容'); ?></label>
